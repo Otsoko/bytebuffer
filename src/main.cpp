05ab794158952220e83990316d3b2e6a3d86e9fe
Otsoko <jfernek@gmail.com>
2023-03-21 18:00:48 +0100

    Namespace std
    Removed [using namespace std] to avoid ambiguity between [typedef unsigned char byte] and [std::byte].

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,6 @@
 #include <iostream>
 #include <stdlib.h>
 
-using namespace std;
-
 void        printBB(ByteBuffer *byteBuffer);
 ByteBuffer *test(ByteBuffer *b);
 
@@ -14,7 +12,7 @@ int main(int argc, char const *argv[]) {
     ByteBuffer bytebuffer(37);
 
     // Comprobamos que el tamaño es el que le hemos dado
-    cout << "Tamaño del buffer: " << bytebuffer.getSize() << " bytes" << endl;
+    std::cout << "Tamaño del buffer: " << bytebuffer.getSize() << " bytes" << std::endl;
 
     bytebuffer.order(BB_LITTLE_ENDIAN);
 
@@ -61,9 +59,9 @@ int main(int argc, char const *argv[]) {
     // Leemos una cadena
     char *str = bytebuffer.getString(6);
 
-    cout << fixed << "Valores leídos: " << (int) b1 << ", " << (int) b2 << ", "
+    std::cout << std::fixed << "Valores leídos: " << (int) b1 << ", " << (int) b2 << ", "
          << (int) b3 << ", " << (int) s << ", " << (int) in << ", " << (float) f
-         << ", " << (Long) l << ", " << (double) d << ", " << str << endl;
+         << ", " << (Long) l << ", " << (double) d << ", " << str << std::endl;
 
     free(str);
 
@@ -76,45 +74,45 @@ int main(int argc, char const *argv[]) {
     // Insertamos un short en la posición 2
     bytebuffer.putShortAt(55, 2);
     // Leemos un short en la posición 2
-    cout << (int) bytebuffer.getShortAt(2) << endl;
+    std::cout << (int) bytebuffer.getShortAt(2) << std::endl;
     // Leemos un int en la posición 5
-    cout << (int) bytebuffer.getIntAt(5) << endl;
+    std::cout << (int) bytebuffer.getIntAt(5) << std::endl;
     // Insertamos un int en la posición 2
     bytebuffer.putIntAt(78945, 2);
     // Leemos un int en la posición 2
-    cout << (int) bytebuffer.getIntAt(2) << endl;
+    std::cout << (int) bytebuffer.getIntAt(2) << std::endl;
     // Insertamos un float en la posición 2
     bytebuffer.putFloatAt(123.45f, 2);
     // Leemos un float en la posición 2
-    cout << (float) bytebuffer.getFloatAt(2) << endl;
+    std::cout << (float) bytebuffer.getFloatAt(2) << std::endl;
     // Insertamos un long en la posición 2
     bytebuffer.putLongAt(789456123LL, 2);
     // Leemos un long en la posición 2
-    cout << (Long) bytebuffer.getLongAt(2) << endl;
+    std::cout << (Long) bytebuffer.getLongAt(2) << std::endl;
     // Insertamos un double en la posición 2
     bytebuffer.putDoubleAt(45678.456123, 2);
     // Leemos un double en la posición 2
-    cout << fixed << bytebuffer.getDoubleAt(2) << endl;
+    std::cout << std::fixed << bytebuffer.getDoubleAt(2) << std::endl;
     // Insertamos una cadena en la posición 2
     bytebuffer.putStringAt("Mundukorik politena", 19, 2);
     // Leemos una cadena en la posición 2
     str = bytebuffer.getStringAt(19, 2);
-    cout << str << endl;
+    std::cout << str << std::endl;
     free(str);
 
     byte *bytes = bytebuffer.getBytes();
     for (size_t i = 0; i < bytebuffer.getSize(); i++) {
-        cout << (int) bytes[i] << " ";
+        std::cout << (int) bytes[i] << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
 
     char *hex1 = bytebuffer.getHexString();
     char  hex2[bytebuffer.getSize() * 2 + 1];
 
     bytebuffer.getHexString(hex2);
 
-    cout << "HexString 1: " << hex1 << endl;
-    cout << "HexString 2: " << hex2 << endl;
+    std::cout << "HexString 1: " << hex1 << std::endl;
+    std::cout << "HexString 2: " << hex2 << std::endl;
 
     // Liberar el devuelto
     free(hex1);
@@ -132,11 +130,11 @@ int main(int argc, char const *argv[]) {
     printBB(&bytebuffer);
     printBB(&cloned);
 
-    cout << endl
+    std::cout << std::endl
          << "s(int): " << sizeof(int) << ", s(long): " << sizeof(long)
          << ", s(long long): " << sizeof(long long)
          << ", s(float): " << sizeof(float) << ", s(double): " << sizeof(double)
-         << ", s(u_int64_t): " << sizeof(u_int64_t) << endl;
+         << ", s(u_int64_t): " << sizeof(u_int64_t) << std::endl;
 
     ByteBuffer *r = NULL;
     r             = test(r);
@@ -151,9 +149,9 @@ int main(int argc, char const *argv[]) {
 void printBB(ByteBuffer *byteBuffer) {
     // Mostramos el contenido del ByteBuffer
     for (size_t i = 0; i < byteBuffer->getSize(); i++) {
-        cout << (int) byteBuffer->getAt(i) << " ";
+        std::cout << (int) byteBuffer->getAt(i) << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
 }
 
 ByteBuffer *test(ByteBuffer *b) {
